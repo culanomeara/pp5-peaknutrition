25d6223a5823323d126d7bbff5c101f43afd821e
Culan O'Meara <culanomeara@gmail.com>
2023-05-19 21:24:03 +0000

    Deploy to heroku

--- peaknutrition/settings.py
+++ peaknutrition/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ex*1jl54c6xf%q5sphys&4)x+!t%9+5ep-@z^@nrjrj6p-6j0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-culanomeara-pp5peaknutr-06r9ptkluv2.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['peak-nutrition.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -102,13 +103,17 @@ WSGI_APPLICATION = 'peaknutrition.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

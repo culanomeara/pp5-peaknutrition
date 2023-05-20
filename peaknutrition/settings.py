132915c96aa1187cbe1bef2deebf665447828cd3
Culan O'Meara <culanomeara@gmail.com>
2023-05-20 20:26:46 +0000

    Add env.py and update secret keys

--- peaknutrition/settings.py
+++ peaknutrition/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from os import path
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ex*1jl54c6xf%q5sphys&4)x+!t%9+5ep-@z^@nrjrj6p-6j0&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

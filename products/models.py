8ca773ac314b35ee75cdfd5869a7c0bf7ff44036
Culan O'Meara <culanomeara@gmail.com>
2023-05-23 18:55:10 +0000

    Removed image)_url from products model

--- products/models.py
+++ products/models.py
@@ -35,7 +35,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 

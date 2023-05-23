9d24a3a9ad6664d545ecf331a749d9122590c2c5
Culan O'Meara <culanomeara@gmail.com>
2023-05-23 14:15:58 +0000

    Added list displays to admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 from .models import Product, Category, Type
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Type)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'type'
+        'price',
+        'rating',
+        'featured_image',
+        'tags',
+    )
+
+    ordering = ('type',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Type, TypeAdmin)

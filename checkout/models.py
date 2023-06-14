e177d5312f7feb00d7b5a9e967c2114d86ab1eb3
Culan O'Meara <culanomeara@gmail.com>
2023-06-14 10:25:44 +0000

    Added order summary to checkout_success.html

--- checkout/models.py
+++ checkout/models.py
@@ -65,7 +65,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = (self.product.price - self.product.price * (self.product.discount/100)) * self.quantity
+        self.lineitem_total = self.product.price * self.quantity
         self.lineitem_discount_total = self.product.price * (self.product.discount/100) * self.quantity
         super().save(*args, **kwargs)
 

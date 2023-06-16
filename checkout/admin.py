cf55fdcbe3450334bb1ed46b9f0529927d81b89d
Culan O'Meara <culanomeara@gmail.com>
2023-06-16 17:16:27 +0000

    Completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,13 +11,15 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date', 'order_total',
-                       'discount_total', 'grand_total',)
+                       'discount_total', 'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode_eircode', 'town_or_city', 'street_address1',
               'street_address2', 'county',
-              'order_total', 'discount_total', 'grand_total',)
+              'order_total', 'discount_total', 'grand_total',
+              'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'discount_total',
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -23,15 +23,18 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
+                payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
+        print(e)
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print(e)
         return HttpResponse(status=400)
     except Exception as e:
+        print(e)
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler

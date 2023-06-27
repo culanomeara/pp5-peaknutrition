e17aca544843f362d8bed940084b431aa11c08ab
Culan O'Meara <culanomeara@gmail.com>
2023-06-27 18:15:15 +0000

    Fixed path error with about.html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-def index(request):
-    """ A view to return the index page """
 
+def home(request):
+    """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+    return render(request, 'home/about.html')

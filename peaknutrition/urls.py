7b058914160858f5c8aedbaeb49081f94468d8ba
Culan O'Meara <culanomeara@gmail.com>
2023-06-27 13:41:07 +0000

    Add about.html to root dir

--- peaknutrition/urls.py
+++ peaknutrition/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('about.html', TemplateView.as_view(template_name='about.html')),
     path('products/', include('products.urls')),
     path('contact/', include('contact.urls')),
     path('bag/', include('bag.urls')),

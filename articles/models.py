2dfe7b7713f3513b6880184fd5ab170542a42639
Culan O'Meara <culanomeara@gmail.com>
2023-06-30 12:24:13 +0000

    Fixed error with contrib.auth settings

--- articles/models.py
+++ articles/models.py
@@ -8,15 +8,10 @@ class Article(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="article"
     )
-    # category = models.ForeignKey(
-    #     'Category', on_delete=models.CASCADE, related_name="post_categories")
     excerpt = models.TextField(blank=True)
     featured_image = models.ImageField(default='default.jpg')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-
-    # class Meta:
-    #     ordering = ["-created_on"]
 
     def __str__(self):
         return self.title

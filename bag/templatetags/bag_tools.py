9ea5e3e1275951f61a630bced721e16afeb3f62b
Culan O'Meara <culanomeara@gmail.com>
2023-07-10 07:47:52 +0000

    Refactored bag and bag_tools code

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -5,30 +5,42 @@ register = template.Library()
 
 
 # https://stackoverflow.com/questions/39021159/django-template-send-two-arguments-to-template-tag
-@register.simple_tag
-def calc_subtotal(price, quantity):
-    global tot
-    tot = round(price * quantity, 0)
-    return tot
+
+# @register.simple_tag
+# def calc_item_subtotal(price, quantity):
+#     # Function for calculating subtotal for each line item in bag
 
 
 @register.simple_tag
-def calc_discount_price(price, discount):
+def calc_item_discount_price(price, discount):
+    # Function for calculating discount price for each line item in bag
     discount_price = round(price - (price * discount/100), 0)
     return discount_price
 
 
 @register.simple_tag
-def calc_discount_subtotal(price, discount, quantity):
+def calc_item_subtotal(price, discount, quantity):
+    # Function for calculating total cost for each line item in bag
+    global tot
     global disc_tot
-    disc_tot = round(price * discount/100 * quantity, 0)
-    return disc_tot
+    tot = 0
+    disc_tot = 0
 
+    if discount != 0:
+        tot = round(price * quantity, 0)
+        disc_tot = round(price * discount/100 * quantity, 0)
+    else:
+        tot = round(price * quantity, 0)
 
-@register.simple_tag
-def calc_item_total():
     item_total = tot - disc_tot
     return item_total
+
+
+# @register.simple_tag
+# def calc_item_total():
+#     # Function for calculating subtotal for each line item in bag
+#     item_total = 
+#     return item_total
 
 
 @register.simple_tag
